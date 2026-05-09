ffd37a9976570654104ee8c8edefc0cd9809a8a9
shobonneko1992-alt <shobon.neko1992@gmail.com>
2026-05-09 10:14:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE = "dayservice-v1";
-const ASSETS = ["/", "/index.html", "/manifest.json"];
+const CACHE = "dayservice-v3";
 
 self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(res) {
-      return res || fetch(e.request);
+    caches.open(CACHE).then(function(cache) {
+      return cache.match(e.request).then(function(res) {
+        return res || fetch(e.request).then(function(response) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
